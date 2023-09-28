6c4f9e9a857a7faf8a0c84dba5423a28a8facfa7
Gabriel Diogo <gabriel.san.diogo@gmail.com>
2023-09-27 21:22:49 -0300

    update formatting values

--- src/modules/generateJson.js
+++ src/modules/generateJson.js
@@ -17,18 +17,18 @@ const resultJson = (xml) =>{
             );
             
             let formated = {
-                clientes: element['ns3:TomadorServico'][0]['ns3:RazaoSocial'][0],
-                data: element['ns3:Competencia'][0],
-                nfse: element['ns3:IdentificacaoNfse'][0]['ns3:Numero'][0],
-                valor: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorServicos') ? element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorServicos'][0].replace('.',',') : "0",
-                pis: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorPis') ? element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorPis'][0].replace('.',',') : "0",
-                cofins: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorCofins') ? element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorCofins'][0].replace('.',',') : "0",
-                ir: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorIr') ? element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorIr'][0].replace('.',',') : "0",
-                inss: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorInss') ? element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorInss'][0].replace('.',',') : "0",
-                csll: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorCsll') ? element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorCsll'][0].replace('.',',') : "0",
-                issRetido: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorIssRetido') ? element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorIssRetido'][0].replace('.',',') : "0",
-                totalRetencao: somaRetencao.toString().replace('.',','),
-                valorLiquido: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorLiquidoNfse') ? element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorLiquidoNfse'][0].replace('.',',') : "0"
+                CLIENTES: element['ns3:TomadorServico'][0]['ns3:RazaoSocial'][0],
+                DATA: element['ns3:Competencia'][0],
+                NFSE: element['ns3:IdentificacaoNfse'][0]['ns3:Numero'][0],
+                VALOR: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorServicos') ? 'R$ ' + element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorServicos'][0].replace('.',',') : "0",
+                'RETENÇÕES PIS': element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorPis') ? 'R$ ' + element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorPis'][0].replace('.',',') : "0",
+                COFINS: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorCofins') ? 'R$ ' + element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorCofins'][0].replace('.',',') : "0",
+                IR: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorIr') ? 'R$ ' + element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorIr'][0].replace('.',',') : "0",
+                INSS: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorInss') ? 'R$ ' + element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorInss'][0].replace('.',',') : "0",
+                CSLL: element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorCsll') ? 'R$ ' + element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorCsll'][0].replace('.',',') : "0",
+                'ISS RETIDO': element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorIssRetido') ? 'R$ ' + element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorIssRetido'][0].replace('.',',') : "0",
+                'TOTAL DA RETENÇÃO': 'R$ ' + somaRetencao.toString().replace('.',','),
+                'LIQUIDO DA NF': element['ns3:Servico'][0]['ns3:Valores'][0].hasOwnProperty('ns3:ValorLiquidoNfse') ? 'R$ ' + element['ns3:Servico'][0]['ns3:Valores'][0]['ns3:ValorLiquidoNfse'][0].replace('.',',') : "0"
             };
     
             NF.push(formated);
